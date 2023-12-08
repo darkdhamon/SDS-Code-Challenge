bbc5e4a3f17ceba225bf748f24348e1e0aff184d
Bronze Brown <dhamon.nightstar@gmail.com>
2023-12-08 15:42:27 -0500

    Update SyncDebug.cs
    Update comments and complete sync code challenge.

--- Syncing/SyncDebug.cs
+++ Syncing/SyncDebug.cs
@@ -15,8 +15,9 @@ namespace DeveloperSample.Syncing
         /// I knew the reason this was failing was due to the async code not getting awaited
         /// properly but having never used Parallel before I needed help.
         /// I initially was going to use google to research this issue but since I have been
-        /// experimenting with AI I decided to see what ChatGPT could come up with. here is a
-        /// link to my conversation with ChatGPT about this method.
+        /// experimenting with AI I decided to see what ChatGPT could come up with.
+        ///
+        /// here is a link to my conversation with ChatGPT about this method.
         /// https://chat.openai.com/share/0943adda-5a1f-467d-9b8b-024debdd4ff4
         /// </summary>
         /// <param name="items"></param>
@@ -43,15 +44,33 @@ namespace DeveloperSample.Syncing
             return list;
         }
 
+        /// <summary>
+        /// I also was not sure how to do this one and used ChatGPT for ideas. In the following code
+        /// "Enumerable.Range(0, 3).Select(i..." I did not know what "i" was but I should have realized
+        /// that it was the index of thread. created by the Enumerable. There was a few options that I
+        /// discussed with ChatGPT about this problem.
+        ///
+        /// After understanding what "i" was I decided to modify the code further so that if we were to modify
+        /// the number of threads in the future we would only need to change one line of code as coding best
+        /// practices would indicate.
+        ///
+        /// here is a link to my conversation with ChatGPT about how to address this issue.
+        /// https://chat.openai.com/share/f4a6e7a4-d38f-4c63-a7ff-5d991b4e1f5a
+        /// </summary>
+        /// <param name="getItem"></param>
+        /// <returns></returns>
         public Dictionary<int, string> InitializeDictionary(Func<int, string> getItem)
         {
             var itemsToInitialize = Enumerable.Range(0, 100).ToList();
 
             var concurrentDictionary = new ConcurrentDictionary<int, string>();
-            var threads = Enumerable.Range(0, 3)
-                .Select(i => new Thread(() => {
-                    foreach (var item in itemsToInitialize)
+            var numThreads = 3;
+            var threads = Enumerable.Range(0, numThreads)
+                .Select(threadID => new Thread(() =>
+                {
+                    for (var index = threadID; index < itemsToInitialize.Count; index+=numThreads)
                     {
+                        var item = itemsToInitialize[index];
                         concurrentDictionary.AddOrUpdate(item, getItem, (_, s) => s);
                     }
                 }))
