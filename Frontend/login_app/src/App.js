ba8e2d0a9110814e3737441ac4f5e21939031c10
Bronze Brown <dhamon.nightstar@gmail.com>
2023-12-08 15:04:21 -0500

    Multiple changes
     - Ignore Node_Modules
     - Upgrade Node Packages
     - First attempt add React model binding worked for the form. but having trouble transfering the information to the other component.
     - Remove skip on sync

--- Frontend/login_app/src/App.js
+++ Frontend/login_app/src/App.js
@@ -4,11 +4,20 @@ import LoginForm from './LoginForm';
 import LoginAttemptList from './LoginAttemptList';
 
 const App = () => {
-  const [loginAttempts, setLoginAttempts] = useState([]);
+    /**
+     * Here I am not sure what to do with setLoginAttempts yet. 
+     * However I am going to use loginAttempts as a list
+     */
+    const [loginAttempts, setLoginAttempts] = useState([]);
 
   return (
     <div className="App">
-      <LoginForm onSubmit={({ login, password }) => console.log({ login, password })} />
+          <LoginForm onSubmit={({ login, password }) => {
+              loginAttempts.push({
+                  login, password
+              });
+              console.log({ login, password })
+          }} />
       <LoginAttemptList attempts={loginAttempts} />
     </div>
   );
--- Frontend/login_app/src/LoginForm.js
+++ Frontend/login_app/src/LoginForm.js
@@ -1,23 +1,44 @@
 import React from "react";
 import './LoginForm.css';
 
+/**
+ * I have no experience with react, but from research online this would be a component
+ * Although it is not built like the ones I am finding on stackoverflow. I know that I
+ * need to create a variables to bind the data too. So for now I will do this like I would
+ * in standard JS and see what happens. 
+ */
+
+
 const LoginForm = (props) => {
+
+	const loginData = {
+		username: "",
+		password: ""
+	};
+
 	const handleSubmit = (event) =>{
 		event.preventDefault();
 
 		props.onSubmit({
-			login: undefined,
-			password: undefined,
+			login: loginData.username,
+			password: loginData.password
 		});
+	}
+
+	const handleUsernameUpdate = (event) => {
+		loginData.username = event.target.value;
+	}
+	const handlePasswordUpdate = (event) => {
+		loginData.password = event.target.value;
 	}
 
 	return (
 		<form className="form">
 			<h1>Login</h1>
 			<label htmlFor="name">Name</label>
-			<input type="text" id="name" />
+			<input type="text" id="name" value={loginData.username} onChange={ handleUsernameUpdate } />
 			<label htmlFor="password">Password</label>
-			<input type="password" id="password" />
+			<input type="password" id="password" value={loginData.password} onChange={ handlePasswordUpdate} />
 			<button type="submit" onClick={handleSubmit}>Continue</button>
 		</form>
 	)
