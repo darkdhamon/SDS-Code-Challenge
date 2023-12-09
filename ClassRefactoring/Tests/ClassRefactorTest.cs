df4e2bcca5d843d67bbb3721d2f13ed92da0f1ac
Bronze Brown <dhamon.nightstar@gmail.com>
2023-12-09 14:53:02 -0500

    Update ClassRefactorTest.cs
     - Reduce Duplicate code in tests

--- ClassRefactoring/Tests/ClassRefactorTest.cs
+++ ClassRefactoring/Tests/ClassRefactorTest.cs
@@ -7,35 +7,49 @@ namespace DeveloperSample.ClassRefactoring.Tests
 {
     public class ClassRefactorTest
     {
-        [Fact]
-        public void AfricanSwallowHasCorrectSpeed()
+        //[Fact]
+        //public void AfricanSwallowHasCorrectSpeed()
+        //{
+        //    var swallow = SwallowFactory.GetSwallow(SwallowType.African);
+        //    Assert.Equal(22, swallow.GetAirspeedVelocity());
+        //}
+
+        //[Fact]
+        //public void LadenAfricanSwallowHasCorrectSpeed()
+        //{
+        //    var swallow = SwallowFactory.GetSwallow(SwallowType.African);
+        //    swallow.ApplyLoad(SwallowLoad.Coconut);
+        //    Assert.Equal(18, swallow.GetAirspeedVelocity());
+        //}
+
+        //[Fact]
+        //public void EuropeanSwallowHasCorrectSpeed()
+        //{
+        //    var swallow = SwallowFactory.GetSwallow(SwallowType.European);
+        //    Assert.Equal(20, swallow.GetAirspeedVelocity());
+        //}
+
+        //[Fact]
+        //public void LadenEuropeanSwallowHasCorrectSpeed()
+        //{
+        //    var swallow = SwallowFactory.GetSwallow(SwallowType.European);
+        //    swallow.ApplyLoad(SwallowLoad.Coconut);
+        //    Assert.Equal(16, swallow.GetAirspeedVelocity());
+        //}
+
+        [Theory]
+        [InlineData(SwallowType.African, SwallowLoad.None, 22)]
+        [InlineData(SwallowType.African, SwallowLoad.Coconut, 18)]
+        [InlineData(SwallowType.European, SwallowLoad.None, 20)]
+        [InlineData(SwallowType.European, SwallowLoad.Coconut, 16)]
+        public void LadenSwallowHasCorrectSpeed(SwallowType swallowType, SwallowLoad swallowLoad, int expectedAirspeed)
         {
-            var swallow = SwallowFactory.GetSwallow(SwallowType.African);
-            Assert.Equal(22, swallow.GetAirspeedVelocity());
+            var swallow = SwallowFactory.GetSwallow(swallowType);
+            swallow.ApplyLoad(swallowLoad);
+            Assert.Equal(expectedAirspeed, swallow.GetAirspeedVelocity());
         }
 
-        [Fact]
-        public void LadenAfricanSwallowHasCorrectSpeed()
-        {
-            var swallow = SwallowFactory.GetSwallow(SwallowType.African);
-            swallow.ApplyLoad(SwallowLoad.Coconut);
-            Assert.Equal(18, swallow.GetAirspeedVelocity());
-        }
-
-        [Fact]
-        public void EuropeanSwallowHasCorrectSpeed()
-        {
-            var swallow = SwallowFactory.GetSwallow(SwallowType.European);
-            Assert.Equal(20, swallow.GetAirspeedVelocity());
-        }
-
-        [Fact]
-        public void LadenEuropeanSwallowHasCorrectSpeed()
-        {
-            var swallow = SwallowFactory.GetSwallow(SwallowType.European);
-            swallow.ApplyLoad(SwallowLoad.Coconut);
-            Assert.Equal(16, swallow.GetAirspeedVelocity());
-        }
+        
         
         [Theory]
         [InlineData(SwallowType.African)]
