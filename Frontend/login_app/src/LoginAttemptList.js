e09ed57f145c524573baff0db252b199bd295d08
Bronze Brown <dhamon.nightstar@gmail.com>
2023-12-09 16:01:42 -0500

    Update LoginAttemptList.js
    - implemented missing filter code.

--- Frontend/login_app/src/LoginAttemptList.js
+++ Frontend/login_app/src/LoginAttemptList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginAttemptList.css";
 
 const LoginAttempt = ({ attempt }) => (
@@ -7,16 +7,39 @@ const LoginAttempt = ({ attempt }) => (
 	</li>
 );
 
-const LoginAttemptList = ({ attempts }) => (
-	<div className="Attempt-List-Main">
-	 	<p>Recent activity</p>
-	  	<input type="input" placeholder="Filter..." />
-		<ul className="Attempt-List">
-			{attempts.map((attempt) => (
-				<LoginAttempt attempt={attempt}></LoginAttempt>
-			))}
-		</ul>
-	</div>
-);
+const LoginAttemptList = ({ attempts }) => {
+
+	const [data, setData] = useState({
+		filter: ""
+	});
+
+	const handleFilterUpdate = (event) => {
+		setData({ ...data, filter: event.target.value.toLowerCase() })
+	};
+
+	return (
+
+		<div className="Attempt-List-Main">
+			<p>Recent activity</p>
+			<input type="input" placeholder="Filter..." value={data.filter} onChange={handleFilterUpdate} />
+			<ul className="Attempt-List">
+				{
+					attempts
+						// search filter by login and password
+						.filter(
+							(attempt) =>
+								attempt.login.toLowerCase().includes(data.filter) ||
+								attempt.password.toLowerCase().includes(data.filter))
+						// map function
+						.map(
+							(attempt) => (
+								<LoginAttempt attempt={attempt}></LoginAttempt>
+							)
+					)
+				}
+			</ul>
+		</div>
+	)
+};
 
 export default LoginAttemptList;
