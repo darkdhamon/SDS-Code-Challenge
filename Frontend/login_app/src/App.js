10ee2f1bb1765fca2d9d628cf780d33817f02166
Bronze Brown <dhamon.nightstar@gmail.com>
2023-12-09 11:27:13 -0500

    Fixed the code related to LoginAttemps.

--- Frontend/login_app/src/App.js
+++ Frontend/login_app/src/App.js
@@ -4,23 +4,20 @@ import LoginForm from './LoginForm';
 import LoginAttemptList from './LoginAttemptList';
 
 const App = () => {
-    /**
-     * Here I am not sure what to do with setLoginAttempts yet. 
-     * However I am going to use loginAttempts as a list
-     */
     const [loginAttempts, setLoginAttempts] = useState([]);
 
-  return (
-    <div className="App">
-          <LoginForm onSubmit={({ login, password }) => {
-              loginAttempts.push({
-                  login, password
-              });
-              console.log({ login, password })
-          }} />
-      <LoginAttemptList attempts={loginAttempts} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <LoginForm onSubmit={({ login, password }) => {
+                const updatedLoginAttempts = [...loginAttempts, { login, password }];
+                setLoginAttempts(updatedLoginAttempts)
+                console.log({ login, password });
+                console.log("all attempts");
+                console.log(loginAttempts);
+            }} />
+            <LoginAttemptList attempts={loginAttempts} />
+        </div>
+    );
 };
 
 export default App;
